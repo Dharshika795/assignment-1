30f3b79af087d40d968dc6ee2eab7da98f5db861
Dharshika795 <146409411+Dharshika795@users.noreply.github.com>
2025-10-31 19:27:23 +0530

    push

---
+++ Day19/Task1.js
@@ -0,0 +1,4 @@
+//Print numbers from 1 to 20 using a for loop.
+for (let i = 1; i <= 20; i++) {
+  console.log(i);
+}
---
+++ Day19/Task4.js
@@ -0,0 +1,6 @@
+//Use do...while to print numbers 1 to 5. java script short version 
+let i = 1;
+do {
+  console.log(i);
+  i++;
+} while (i <= 5);
